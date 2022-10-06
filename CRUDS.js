a200b622d639579ec659435924b80805b2c792b4
unknown <mahmoudbathish23@gmail.com>
2022-10-06 12:32:27 +0300

    setup the functions

--- CRUDS.js
+++ CRUDS.js
@@ -1,1 +1,52 @@
-//32:30
+
+let title = document.getElementById('title');
+let price = document.getElementById('price');
+let taxes = document.getElementById('taxes');
+let ads = document.getElementById('ads');
+let discount = document.getElementById('discount');
+let total = document.getElementById('total');
+let count = document.getElementById('count');
+let category = document.getElementById('category');
+let submit = document.getElementById('submit');
+
+//get total
+function getTotal(){
+    if(price.value != ''){
+        let result = (+price.value + +taxes.value + +ads.value ) - +discount.value;
+        total.innerHTML = result;
+        total.style.background = '#040';
+    }else {
+        total.innerHTML = '';
+        total.style.background = '#a00d02'
+    }
+}
+//create product
+let dataPro = [];
+
+submit.onclick = function (){
+    let newPro = {
+        title: title.value,
+        price: price.value,
+        taxes: taxes.value,
+        ads: ads.value,
+        discount: discount.value,
+        total: total.innerHTML,
+        count:count.value,
+        category:category.value, 
+    }
+}
+
+
+
+
+
+
+//save in local storage
+//clear inputs
+//read
+//count
+//delete
+//update
+//search
+//clean data
+
